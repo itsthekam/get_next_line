25d9946b93c247fd9a00775cbcebe6ea0f25b1fe
Kamil El Malti <kel-malt@paul-f3Ar2s2.clusters.42paris.fr>
2024-03-25 17:19:30 +0100

    r

--- get_next_line.c
+++ get_next_line.c
@@ -185,19 +185,19 @@ char	*get_next_line(int fd)
 	return (ret);
 }
 
-int	main(void)
-{
-	int	fd;
-	char	*line;
+// int	main(void)
+// {
+// 	int	fd;
+// 	char	*line;
 
-	fd = open("test", O_RDONLY);
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (line == NULL)
-			break;
-		printf("%s", line);
-		free(line);
-	}
-	return (0);
-}
+// 	fd = open("test", O_RDONLY);
+// 	while (1)
+// 	{
+// 		line = get_next_line(fd);
+// 		if (line == NULL)
+// 			break;
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	return (0);
+// }
